d104b29bfbab02f7a7b036b8162e6eb006362928
javier Fernandez <javierfg410@gmail.com>
2021-04-23 19:37:20 +0200

    Error migration corregido 3

--- database/migrations/2021_04_22_164923_create_role_users_table.php
+++ database/migrations/2021_04_22_164923_create_role_users_table.php
@@ -14,7 +14,9 @@ class CreateRoleUsersTable extends Migration
     public function up()
     {
         Schema::create('role_users', function (Blueprint $table) {
-            $table->id('id_role','id_user');
+            $table->bigIncrements('id_role');
+            $table->bigIncrements('id_user');
+            $table->primary(['id_role','id_user']);
             
             $table->foreign('id_role')->references('id_role')->on('roles');
             $table->foreign('id_user')->references('id')->on('users');
