a62c143130ca5a1df1aecbc590d3779261d24a68
javier Fernandez <javierfg410@gmail.com>
2021-04-23 14:57:01 +0200

    Añadido Seeding de usuarios y roles

---
+++ database/seeders/Roles.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class Roles extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'id' => '1',
+            'name' => 'Admin',
+            'description' => 'This user can control everything.',
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => '2',
+            'name' => 'Customer ',
+            'description' => 'This user only controls their restaurants.',
+        ]);
+    }
+}
---
+++ database/seeders/UserAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserAdmin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id' => '1',
+            'name' => 'Admin',
+            'lastname' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
